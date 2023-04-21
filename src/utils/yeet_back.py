bc1165dba8f68b5863bb9ad04779013569ccd6f3
Bentley Hensel <bentley.hensel@civicactions.com>
2023-04-21 16:12:10 +0000

    Yeeting back :)

---
+++ src/utils/yeet_back.py
@@ -0,0 +1,8 @@
+import time
+from utils.watch import logger
+
+
+def axe_catcher(streamlined_results, url_id):
+    logger.debug(f'Bringing the Yeet Back for {url_id} !')
+    logger.debug(f'Prepping {streamlined_results}')
+    time.sleep(15)
